7f9fa299cb9b40648933c710b6fab756ac8d440e
INFO1-EQUIP08\Tarda <a24vicmilsal@inspedralbes.cat>
2025-11-11 19:02:41 +0100

    añadimos el cambio entre menus, puedes cambiar entre botones pero hay algunos errores,arreglar

---
+++ Assets/OptionMenuController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionMenuController : MonoBehaviour
+{
+    [SerializeField] GameObject[] panel;
+    GameObject currentPanel;
+    Button button;
+
+    private void Start()
+    {
+        currentPanel = panel[0];
+        panel[0].SetActive(true);
+    }
+
+
+    public void ChangeMenu(GameObject activePanel)
+    {
+        activePanel.SetActive(true);
+        currentPanel.SetActive(false);
+        currentPanel = activePanel;
+        button.interactable = false;
+    }
+
+
+}
