4ac16560adabc6160fb7d9575cff35093594d9c5
Blascard <blascard02@gmail.com>
2025-11-21 16:03:28 +0100

    Añadida funcion para rotar un objeto 3D con input del ratón

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance;
+
+    public InputActionAsset map;
+
+    public InputAction leftClick_ia;
+    public InputAction mouseLook_ia;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+
+        DontDestroyOnLoad(this);
+
+        EnableInput();
+
+        leftClick_ia = map.FindActionMap("MouseControl").FindAction("Left Click");
+        mouseLook_ia = map.FindActionMap("MouseControl").FindAction("Mouse Look");
+    }
+
+    public void EnableInput()
+    {
+        map.Enable();
+    }
+
+    public void DisableInput()
+    {
+        map.Disable();
+    }
+}
---
+++ Assets/Scripts/Rotate3DObject.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Rotate3DObject : MonoBehaviour
+{
+    private InputManager inputManager;
+
+    private bool rotationAllowed;
+    private Camera currentCamera;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private bool invertedControl = false;
+
+    private void Awake()
+    {
+        inputManager = InputManager.instance;
+
+        if (inputManager.leftClick_ia != null)
+        {
+            inputManager.leftClick_ia.started += OnLeftClickPressed;
+            inputManager.leftClick_ia.performed += OnLeftClickPressed;
+            inputManager.leftClick_ia.canceled += OnLeftClickPressed;
+        }
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        currentCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (!rotationAllowed)
+        {
+            return;
+        }
+
+        Vector2 mouseDelta = GetMouseLookInput();
+
+        mouseDelta *= rotationSpeed * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * (invertedControl ? 1 : -1), mouseDelta.x, Space.World);
+        transform.Rotate(Vector3.right * (invertedControl ? 1 : -1), mouseDelta.y, Space.World);
+    }
+
+    protected virtual void OnLeftClickPressed(InputAction.CallbackContext context)
+    {
+        if (context.started || context.performed)
+        {
+            rotationAllowed = true;
+        }
+        else if (context.canceled)
+        {
+            rotationAllowed = false;
+        }
+    }
+
+    protected virtual Vector2 GetMouseLookInput()
+    {
+        if (inputManager.mouseLook_ia != null)
+        {
+            return inputManager.mouseLook_ia.ReadValue<Vector2>();
+        }
+
+        return Vector2.zero;
+    }
+}
