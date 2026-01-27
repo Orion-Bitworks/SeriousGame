efb48fa17adc57cee20fcfb4905adbda6fe1a494
GerardBlasco <a24gerblaper@inspedralbes.cat>
2026-01-27 17:28:45 +0100

    Creado script EventUIDrag, para hacer menus que instancien piezas al hacer Drag and Drop

---
+++ Assets/Scripts/EventUIDrag.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EventUIDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private GameObject prefabPiece;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+}
