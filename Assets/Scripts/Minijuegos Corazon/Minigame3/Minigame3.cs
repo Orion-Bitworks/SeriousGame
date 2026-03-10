2be9e5b510abc0870d609fc9c7ec910ffdaa0914
a24irifragra@inspedralbes.cat <a24irifragra@inspedralbes.cat>
2026-03-10 18:34:22 +0100

    Condicion de derrota casi echa ultimo minijuego

--- Assets/Scripts/Minijuegos Corazon/Minigame3/Minigame3.cs
+++ Assets/Scripts/Minijuegos Corazon/Minigame3/Minigame3.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Minigame3 : MonoBehaviour
 {
@@ -24,8 +26,21 @@ public class Minigame3 : MonoBehaviour
     [SerializeField] PopUp popUpManager;
     private bool gameActive = false; //Variable per pausar el joc
 
+    private List<RythmNote> activateNotes = new List<RythmNote>(); //lista per controlar les notes actives
+
+    [SerializeField] public GameObject rebootButton; //boto per reiniciar el minijoc en cas de haver-ho fet malament
+
+    //ELEMENTOS A ESCONDER
+    [SerializeField] private GameObject[] elementsToHide;
+
+
     private void Awake()
     {
+        foreach(GameObject obj in elementsToHide) //amagar tots els elements dins del array
+        {
+            obj.SetActive(false);
+        }
+
         StartCoroutine(ShowInstructions()); //Corrutina per mostrar les instruccions del minijoc
     }
 
@@ -68,6 +83,23 @@ public class Minigame3 : MonoBehaviour
     //Mostrar seguent nota
     void SpawnNextNote()
     {
+
+        if(activateNotes.Count >= 2)
+        {
+            RythmNote oldest = activateNotes[0];
+            activateNotes.RemoveAt(0);
+
+            bool wasCurrent = (oldest == currentNote); //tornem a reasignar la nota que s'haura de clicar
+
+            Destroy(oldest.gameObject); //es destrueix la nota mes antiga (la que ha estat mes temps en pantalla)
+
+            if(wasCurrent)
+            {
+                currentNote = null;
+                TryActivateNext();
+            }
+        }
+
         Vector3 randomPos = new Vector3(
             Random.Range(minSpawn.x, maxSpawn.x),
             Random.Range(minSpawn.y, maxSpawn.y),
@@ -79,10 +111,14 @@ public class Minigame3 : MonoBehaviour
         RythmNote note = obj.GetComponent<RythmNote>();
         note.Init(GetRandomKey(), this);
 
+        activateNotes.Add(note);
+
         noteQueue.Enqueue(note); //posa a la queue la nota (especie de llista)
 
         TryActivateNext();
     }
+
+    
 
     //Activar la seguent nota (sense marcar)
     void TryActivateNext()
@@ -98,6 +134,7 @@ public class Minigame3 : MonoBehaviour
     {
         if (note != currentNote) return;
 
+        activateNotes.Remove(note); //treure la nota de la llista si es completa
         Destroy(note.gameObject);
         currentNote = null;
 
@@ -105,19 +142,61 @@ public class Minigame3 : MonoBehaviour
 
         if (completedNotes >= maxNotes)
         {
-            gameActive = false; // Bloqueja el joc
-            Debug.Log("HAS ACABAT!  Has completat totes les notes correctament!");
-            popUpManager.ShowPopUp("Has acabat el tercer minijoc, Felicitats! Has acabat tots els minijocs correctament", 3f);
+            EndMinigame(); //Al finalitzar el minijoc
             return;
         }
-
-        TryActivateNext();
+            TryActivateNext();
     }
+
+    //Metode per la condicio de finalitzacio del minijoc
+    private void EndMinigame()
+    {
+        gameActive = false;
+
+        if(completedNotes >= maxNotes)
+        {
+            popUpManager.ShowPopUp("Has acabat el tercer minijoc, Felicitats! Has acabat tots els minijocs correctament", 3f);
+        }
+        else
+        {
+            rebootButton.SetActive(true);
+            popUpManager.ShowPopUp($"Has fet un total de {completedNotes} de {maxNotes}, torna a intentar-ho de nou", 4);
+        }
+    }
+
 
     //Treure teclas random per mostrar
     KeyCode GetRandomKey()
     {
         KeyCode[] keys = { KeyCode.A, KeyCode.S, KeyCode.D };
         return keys[Random.Range(0, keys.Length)];
+    }
+
+    public void RestartMinigame()
+    {
+        StopAllCoroutines();
+        gameActive = false;
+
+        spawnedNotes = 0;
+        completedNotes = 0;
+        currentNote = null;
+        noteQueue.Clear();
+
+        foreach(RythmNote n in activateNotes)
+        {
+            if(n != null)
+            {
+                Destroy(n.gameObject);
+            }
+            
+        }
+        activateNotes.Clear();
+
+        foreach(GameObject obj in elementsToHide)
+        {
+            obj.SetActive(false);
+        }
+
+        StartCoroutine(ShowInstructions());
     }
 }
