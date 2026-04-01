d6bbc49196936d8fb4ac309651a1f28e3116ff46
a24irifragra@inspedralbes.cat <a24irifragra@inspedralbes.cat>
2026-04-01 15:43:07 +0200

    Texturas agregadas

--- Assets/Scripts/Minijuegos Corazon/RotateObjects.cs
+++ Assets/Scripts/Minijuegos Corazon/RotateObjects.cs
@@ -6,8 +6,9 @@ public class RotateObjects : MonoBehaviour
 {
     public void rotateObjectsMinigame1(SelectObject objeto)  // Recibe el objeto correcto
     {
+
         if (objeto != null)
-            objeto.transform.Rotate(0, 0, 180f, Space.Self);
+            objeto.transform.Rotate(0, 180f, 0, Space.Self);
     }
 
     public void rotateObjectsMinigame2(SelectObject objeto)
