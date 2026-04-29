8c77aa9cdb060249b8558c5bc005d63facde25e0
GerardBlasco <a24gerblaper@inspedralbes.cat>
2026-04-29 18:22:23 +0200

    Añadida funcion random y otros arreglos al script Infinite Rotation

--- Assets/3DPuzzle/Scripts/InfiniteRotation.cs
+++ Assets/3DPuzzle/Scripts/InfiniteRotation.cs
@@ -4,29 +4,36 @@ using UnityEngine;
 
 public class InfiniteRotation : MonoBehaviour
 {
-    [Header("Rotation axis")]
-    [SerializeField] private bool x = false;
-    [SerializeField] private bool y = false;
-    [SerializeField] private bool z = false;
+    //[Header("Rotation axis")]
+    [HideInInspector, SerializeField] private bool x = false;
+    [HideInInspector, SerializeField] private bool y = true;
+    [HideInInspector, SerializeField] private bool z = false;
 
-    [Header("Rotation speed")]
-    [SerializeField] private float speed = 40f;
+    //[Header("Rotation speed")]
+    [HideInInspector, SerializeField] private float speed = 60f;
 
-    [Header("Rotation parameters")]
-    [SerializeField] private bool inverted = false;
-    [SerializeField] private bool rotateOnCreation = false;
+    //[Header("Rotation parameters")]
+    [HideInInspector, SerializeField] private bool inverted = false;
+    [HideInInspector, SerializeField] private bool rotateOnCreation = true;
+    [HideInInspector, SerializeField] private bool random = false;
 
     private int value = 1;
 
     private bool canRotate = true;
 
     private Vector3 initialRotation;
-
+    private Vector3 randomRotation = Vector3.zero;
     private void Start()
     {
         if (inverted)
         {
             value = -1;
+        }
+
+        if (random)
+        {
+            Debug.Log("He entrado en random");
+            randomRotation = Random.insideUnitSphere;
         }
 
         initialRotation = transform.rotation.eulerAngles;
@@ -36,9 +43,13 @@ public class InfiniteRotation : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (canRotate)
+        if (canRotate && !random)
         {
             transform.Rotate(new Vector3(x ? value : 0, y ? value : 0, z ? value : 0) * speed * Time.deltaTime);
+        }
+        else if (canRotate && random)
+        {
+            transform.Rotate(randomRotation * speed * Time.deltaTime);
         }
     }
 
@@ -50,5 +61,74 @@ public class InfiniteRotation : MonoBehaviour
     public void ResetRotation()
     {
         transform.rotation = Quaternion.Euler(initialRotation);
+    }
+
+    public bool GetX()
+    {
+        return x;
+    }
+
+    public void SetX(bool b)
+    {
+        x = b;
+    }
+
+    public bool GetY()
+    {
+        return y;
+    }
+
+    public void SetY(bool b)
+    {
+        y = b;
+    }
+
+    public bool GetZ()
+    {
+        return z;
+    }
+
+    public void SetZ(bool b)
+    {
+        z = b;
+    }
+
+    public bool GetRandom()
+    {
+        return random;
+    }
+
+    public void SetRandom(bool b)
+    {
+        random = b;
+    }
+    public bool GetInverted()
+    {
+        return inverted;
+    }
+
+    public void SetInverted(bool b)
+    {
+        inverted = b;
+    }
+
+    public bool GetRotateOnCreation()
+    {
+        return rotateOnCreation;
+    }
+
+    public void SetRotateOnCreation(bool b)
+    {
+        rotateOnCreation = b;
+    }
+
+    public float GetSpeed()
+    {
+        return speed;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
     }
 }
